4f9cc6196fc89db6456707b3e16ca0f4b31c0811
SimonSongg <sf995511sf@qq.com>
2021-07-14 21:16:45 +0800

    Update radarpublisher.py and ultrasonic.py

--- src/gpspub/scripts/radarpublisher.py
+++ src/gpspub/scripts/radarpublisher.py
@@ -37,7 +37,7 @@ ubyte_array = c_ubyte*8
 ubyte_3array = c_ubyte*3
 b = ubyte_3array(0, 0 ,0)
 data_array = [0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0]
-data_array1 = [0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0]
+#data_array1 = [0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0]
 CanDLLName = './ControlCAN.dll' #把DLL放到对应的目录下
 #canDLL = windll.LoadLibrary('./ControlCAN.dll')
 #Linux系统下使用下面语句，编译命令：python3 python3.8.0.py
@@ -68,9 +68,9 @@ time_start = 0.0
 a = ubyte_array(0, 0, 0, 0, 0, 0, 0, 0)
 vci_can_obj = VCI_CAN_OBJ(0x0, 0, 0, 0, 0, 0,  0, a, b)#容器
 #vci_can_obj = VCI_CAN_OBJ[2500]
-while True:
+while not rospy.is_shutdown():
     radar = radardata()
-    radar_ori = radardata()
+    #radar_ori = radardata()
     ret = canDLL.VCI_Receive(VCI_USBCAN2, 0, 0, byref(vci_can_obj), 1, 0)
     if ret > 0:
         index = list(vci_can_obj.Data)[1]
@@ -78,13 +78,13 @@ while True:
         #print(distance)
         #print(list(vci_can_obj.Data))
         if (index == 31):
-            data_array1[index] = distance
+            #data_array1[index] = distance
             data_array[index-16] = distance        
             #print(index)
             #print(data_array)
             #radar = copy.deepcopy(data_array)
             radar = data_array #try if it works
-            radar_ori = data_array1
+            #radar_ori = data_array1
             pub.publish(radar)
             #pub.publish(radar_ori)
             
--- src/gpspub/scripts/ultrasonic.py
+++ src/gpspub/scripts/ultrasonic.py
@@ -1,36 +1,55 @@
+#!/usr/bin/env python
 import serial
 import os
+import rospy
+from std_msgs.msg import Float64
 
-os.system('echo %s | sudo -S %s' % ('ADV', 'chmod 777 /dev/ttyUSB2'))
+os.system('echo %s | sudo -S %s' % ('ADV', 'chmod 777 /dev/ultrasonic0'))
 
-ser=serial.Serial("/dev/ttyUSB2",9600,timeout=2)
+ser=serial.Serial("/dev/ultrasonic0",9600,timeout=2)
 #ser.open()
 state = 0 
 data_256 = 0
 data_16 = 0
 
-while True:
+pub = rospy.Publisher('UltraDistance', Float64, queue_size = 5)
+rospy.init_node('Ultrasonic_Publisher', anonymous = False)
+ultra_data = Float64()
+
+while not rospy.is_shutdown():
+    #print(1)
     line = ser.read(1)
-    data = line[0]
+    #print(line)
+    data = int(line.encode('hex'),16)
     #print(data)
+    
+    #print(int(data))
     if data == 255 and state == 0:
         state = 1
+        #print(2)
     elif data == 255 and state == 1:
         state = 0
+        #print(3)
     elif data != 255 and state == 1:
         state = 2
+        #print(4)
         data_256 = data
     elif state == 2:
         state = 3
+        #print(5)
         data_16 = data
     elif state == 3:
         if (data_256 + data_16 + 255) & 255 == data:
+            #print(6)
             print(data_256*256+data_16)
+            ultra_data = (data_256*256+data_16)/1000.0
+            pub.publish(ultra_data)
             state = 0
             data_16 = 0
             data_256 = 0
         else:
             print("error data")
+            #print(7)
             state = 0
             data_16 = 0
             data_256 = 0
